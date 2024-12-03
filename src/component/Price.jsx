fff3ef2d815d0ad87b6945a7b7f610ce093cad7d
Savita Rao <savitarao702@gmail.com>
2024-12-03 22:28:17 +0530

    some changes in price.css and price.jsx

--- src/component/Price.jsx
+++ src/component/Price.jsx
@@ -1,16 +1,19 @@
 import "../styling.css/Price.css"
-export const PriceCard = ({image, heading, para, listIteam, subheading}) => {
-    return(
+export const PriceCard = ({ image, heading, para, listItems, subheading }) => {
+    return (
         <div className="container">
-        <img src={image} alt="A coffee cup"/>
-        <h2>{heading}</h2>
-        <p>{para}</p>
-        <ul>
-            {/* listItems.map(index.iteam)
-              <li>{iteam.index}</li> */}
-        </ul>
-        <h3> starting at  <span>{subheading} </span></h3>
-        <button>DETAILS</button>
+            <img src={image} alt="A coffee cup" />
+            <h2>{heading}</h2>
+            <p>{para}</p>
+            <ul>
+                {
+                    listItems.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))
+                }
+            </ul>
+            <h3> starting at  <span>{subheading} </span></h3>
+            <button>DETAILS</button>
         </div>
 
     )
